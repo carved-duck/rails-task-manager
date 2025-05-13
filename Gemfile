e9f58cd6378764bddc9d4e6efbb85f96517f3c89
Julian Schoenfeld <juliankick@gmail.com>
2025-05-13 15:31:52 +0900

    Created a rails task manager

--- Gemfile
+++ Gemfile
@@ -62,3 +62,5 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+#custom gems
+gem 'ostruct'
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  #create
+  get "/tasks/new", to: "tasks#new", as: :new_task
+  post "/tasks", to: "tasks#create", as: :tasks
+  #Read_All --> Index
+  get "/tasks", to: "tasks#index"
+  #read_one __> show
+  get "/tasks/:id", to: "tasks#show", as: :task
+  #update
+  get "/tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "/tasks/:id", to: "tasks#update"
+  #delete
+  delete "/tasks/:id", to: "tasks#destroy"
 end
